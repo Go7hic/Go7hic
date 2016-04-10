b1800c12b2400e649b8828fcb523d4ff06687dc7
Go7hic <gtfx0209@qq.com>
2016-04-10 12:49:58 +0800

    update

--- src/index.js
+++
@@ -1,7 +0,0 @@
-// import 'es6-promise';
-// import 'isomorphic-fetch';
-import { h, render } from 'preact';
-import App from './components/app';
-import './style';
-
-render(<App />, document.body);
