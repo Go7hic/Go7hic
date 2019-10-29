c8a5d1b062b54aaff7ed517368a9d47ea677aaaf
Go7hic <gtfx0209@qq.com>
2019-10-29 23:22:27 +0800

    update

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -98,9 +98,11 @@ class Index extends React.Component {
         <div class="wrapper">
         <h2>Projects</h2>
         <ul>
-          <li><a target="_blank" href="">krul-cli</a>: a CLI tool for create fe project</li>
+          <li><a target="_blank" href="https://github.com/Go7hic/krul-cli">krul-cli</a>: a CLI tool for create fe project</li>
           <li><a target="_blank" href="https://github.com/R-Components">R-Components</a>: some react components</li>
           <li><a target="_blank" href="https://github.com/RN-Components">RN-Components</a>: some react-native components</li>
+          <li><a target="_blank" href="https://github.com/Go7hic/bomb">bomb</a>: 电话轰炸器桌面客户端</li>
+          <li><a target="_blank" href="https://github.com/Go7hic/xupload">xupload</a>: 阿里云CDN上传桌面客户端</li>
          
         </ul>
         <div>More on my <a target="_blank" href="https://github.com/Go7hic">Github</a></div>
